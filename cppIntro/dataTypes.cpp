ceb906f4ac03c64617cdeb995f80080486ea7d04
Ritik Bhardwaj <varribhar@gmail.com>
2023-05-24 00:13:32 +0530

    add data types in cpp

---
+++ cppIntro/dataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cout << "size of int "<<sizeof(a)<<"\n";
+
+    float b;
+    cout << "size of float "<<sizeof(b)<<endl;
+
+    char c ;
+    cout << "size of char "<<sizeof(c)<<endl;
+
+    bool d;
+    cout << "size of bool "<<sizeof(d)<<endl;
+
+    return 0;
+}
