f6c9b92158febbfe2fa44d880631bfef472c2dd8
Ritik Bhardwaj <varribhar@gmail.com>
2023-05-28 21:47:51 +0530

    add assignment operator

---
+++ cppIntro/operators.cpp
@@ -0,0 +1,73 @@
+// Arithmetic Operators
+   // Binary Operators: +, -, *, /, %;  
+   // Unary Operators: ++, --;
+
+   // Unary Operators:
+        // Preincrementor ++a; 
+        // Postincrementor a++;
+
+
+// Relational Operators: ==, !=, >, <, >=, <=;
+    // returns boolean 
+
+
+// Logical Operators: &&, ||, !
+
+
+// Bitwise Operators:  &, |, ^, ~, <<, >>;
+    // & AND Operator
+        //   0110
+        // & 0100
+        //   -----
+        //   0100 - If both are 1 then it returns 1 else 0;
+
+    // | OR Operator
+        //   0110
+        // | 0100
+        //   -----
+        //   0110 - If any one is 1 then it returns 1 else 0;
+
+    // ^ XOR Operator
+        //   0110
+        // ^ 0100
+        //   -----
+        //   0010 - If two values are same then returns 0 else 1;
+
+    // ~ Ones Complement
+        // ~ 0100
+        //   -----
+        //   1011 - It simply invert 1 with 0 and 0 with 1;
+
+    // << Left Shift Operator
+        //   4 << 1 
+        //   (0100)
+        // = 1000 also equals to 8
+        //   a<<n = a*2^n 
+        //   it shifts every bit to left;
+
+    // >> Right Shift Operator
+        //   4 >> 1 
+        //   (0100)
+        // = 0010 also equals to 2
+        //   a>>n = a/2^n 
+        //   it shifts every bit to left;
+
+
+// Assignment Operators: =, +=, -=, *=, /=;
+
+    
+// Miscelaneous Operators: 
+    // sizeof()
+    // condition? if true then X: if false then Y;
+    // cast - convert one data type into another simply char to int, int to char;
+    // comma operator 
+    // reference operator - returns address of a variable 
+    // * pointer operator int * b=a 
+
+
+
+
+
+
+// Operator Precedence
+
