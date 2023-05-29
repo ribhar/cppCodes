da8683cbcaa26a335ad02cf7df801b7bbf41530b
Ritik Bhardwaj <varribhar@gmail.com>
2023-05-29 23:36:13 +0530

    add more patterns

--- basicPatterns/halfPyramidAfter180Rotation.cpp
+++ basicPatterns/halfPyramidAfter180Rotation.cpp
@@ -6,7 +6,15 @@ int main(){
     int n;
     cin>>n;
 
-    for(int i =0; i<n; i++){
+    for(int i =1; i<=n; i++){
+        int j = 1;
+        for(j; j<=n-i; j++){
+            cout<<"  ";
+        }
+        for(j; j<=n; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
         
     }    
 
