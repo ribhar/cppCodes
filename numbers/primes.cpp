d7eb0d5bf1a6dd70c94c657610b1b955743d717c
Ritik Bhardwaj <varribhar@gmail.com>
2023-06-03 13:14:01 +0530

    add number problems

---
+++ numbers/primes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+bool isPrime(int n){
+
+    for (int i = 2; i < n; i++)
+    {
+        if(n%i==0){
+            return 0;
+        }
+    }
+    return 1;    
+}
+int main(){
+    int a,b;
+    cin>>a>>b;
+
+    for (int i = a; i <=b; i++)
+    {
+        if(isPrime(i)){
+            cout<<i<<endl;
+        }
+    }
+    
+    
+    return 0;
+}
