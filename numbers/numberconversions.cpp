47b6eb288cb03628283ce0414a29346f52677cdf
Ritik Bhardwaj <varribhar@gmail.com>
2023-06-06 23:33:04 +0530

    add number conversion problem

--- numbers/numberconversions.cpp
+++ numbers/numberconversions.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int binaryToDecimal(int n){
@@ -14,10 +14,50 @@ int binaryToDecimal(int n){
     return covertedNumber;
 
 }
+
+int octalToDecimal(int n){
+    int basePower=1, // 8 as base, initial power is 0, 8^0 for starting hence 8^0 is 1.
+    covertedNumber=0;
+
+    while(n>0){
+        int lastDigit = n%10;
+        covertedNumber +=  lastDigit*basePower; // Last digit * 2^power, eg: 1*8^2 | 1*64, 1*8^3 | 1*512.
+        basePower *= 8; // Increse power of base 8, eg 8*8*.......8 .
+        n /= 10;  // Remove one digit from last.
+    }
+    return covertedNumber;
+
+}
+
+int hexadecimalToDecimal(string n){
+    int basePower=1, // 8 as base, initial power is 0, 8^0 for starting hence 8^0 is 1.
+    covertedNumber=0;
+
+    int s = n.size();
+    for (int i = s-1; i>=0; i--)
+    {
+        if(n[i] >= '0' && n[i] <= '9'){
+            covertedNumber += (n[i]-'0')*basePower; // Last digit * 16^power, eg: 1*16^2 | 1*256, F*16^3 | 15*4096.
+        }
+          else if(n[i] >= 'a' && n[i] <= 'f'){
+            covertedNumber += (n[i]-'a'+10)*basePower; // Get alphabets hexa representation, Last digit * 16^power, eg: 1*16^2 | 1*256, F*16^3 | 15*4096.
+        }
+        else if(n[i] >= 'A' && n[i] <= 'F'){
+            covertedNumber += (n[i]-'A'+10)*basePower; // Get alphabets hexa representation, Last digit * 16^power, eg: 1*16^2 | 1*256, F*16^3 | 15*4096.
+        }
+      
+        basePower *= 16; // Increse power of base 16, eg 16*16*.......16 .
+    }
+    return covertedNumber;
+
+}
+
 int main(){
-    int n;
+    string n;
     cin>> n;
 
-    cout<< binaryToDecimal(n)<<endl;
+    // cout<<"Binary "<<n<<" in decimal is "<<binaryToDecimal(n)<<endl;
+    // cout<<"Octal "<<n<<" in decimal is "<< octalToDecimal(n)<<endl;
+    cout<<"Hexadecimal "<<n<<" in decimal is "<< hexadecimalToDecimal(n)<<endl;
     return 0;
 }
