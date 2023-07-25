425e62cb625c840eff532511b60f0e35e67d336d
ribhar <ribharscode@gmail.com>
2023-07-25 23:30:26 +0530

    add isPrime1

--- numbers/isPrime1.cpp
+++ numbers/isPrime1.cpp
@@ -1,1 +1,21 @@
-// test commit 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    bool isPrime = 1;
+
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            cout<<"not a prime number"<<endl;
+            isPrime = 0;
+            break;
+        }
+    }
+    if(isPrime){
+        cout<<"prime number"<<endl;
+    }
+    return 0;
+}
